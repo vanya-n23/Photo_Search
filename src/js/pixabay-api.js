4ad1ddcbd66290c6eceef0dde391d11728af50d0
vanya-n23 <vanya.neresnitsia@gmail.com>
2025-05-08 17:10:30 +0000

    s

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_KEY = '46793752-15e3775c3640735c851618a54';
 const BASE_URL = 'https://pixabay.com/api/';
-const PER_PAGE = 15; 
+const PER_PAGE = 15;
 
 export async function fetchImages(query, page = 1) {
   const url = `${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${PER_PAGE}&page=${page}`;
